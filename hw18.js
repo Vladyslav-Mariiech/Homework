74fe726ebd40533094c3617c797854a73e0d23de
Vladyslav-Mariiech <153525092+Vladyslav-Mariiech@users.noreply.github.com>
2024-03-13 18:50:41 +0200

    Update hw18.js

--- hw18.js
+++ hw18.js
@@ -1,3 +1,4 @@
+//task1
 function validateForm(data) {
     if (!data.email.includes('@')) {
       throw new Error('Wrong email.');
@@ -16,6 +17,37 @@ function validateForm(data) {
     };
     const isValid = validateForm(dataEx);
     console.log('Validate Form:', isValid);
+  } catch (error) {
+    console.error(error.message);
+  }
+
+//task2
+function processArray(array, check) {
+    for (let i = 0; i < array.length; i++) {
+      try {
+        array[i] = check(array[i]);
+      } catch (error) {
+        return {
+          error: error.message,
+          index: i
+        };
+      }
+    }
+    return array;
+  }
+  
+  const myArray = [1, 2, 3, '4', 5];
+  
+  const checkFunction = (el) => {
+    if (typeof el !== 'number') {
+      throw new Error('Not a number.');
+    }
+    return el;
+  };
+  
+  try {
+    const result = processArray(myArray, checkFunction);
+    console.log(result);
   } catch (error) {
     console.error(error.message);
   }
